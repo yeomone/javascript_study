daee1ed14e6904b9acb9618a3ca22a4244bf04a1
won <won00yeom@naver.com>
2023-12-27 18:14:32 +0900

    bom

---
+++ script/bom2.js
@@ -0,0 +1,32 @@
+/* let m1 = window.alert('popup');
+let m2 = window.alert('잠시 기다려주세요');
+let m3 = alert('준비중입니다');
+let m4 = window.alert('잘못 접근하셨습니다');
+let m5 = window.alert('제출 완료되었습니다');
+let m6 = window.alert('결제 실패하였습니다');
+console.log(m1); */
+
+/* const qa1 = window.prompt('당신의 나이는?','');
+console.log(qa1);
+console.log(typeof qa1); */
+
+/* const ok = window.confirm('결제하시겠습니까?');
+console.log(ok); */
+
+// window.print();
+
+function func(){
+    window.location.href="https://naver.com";
+}
+// ---------------------------
+// 객체.속성.속성
+// 객체.속성.메서드()
+window.document.write("테스트 문구");
+document.write('자바스크립트')
+document.write('hello<br>world')
+// -----------------------------------
+// window.open('https://google.com', '_blank')
+function func2(){
+    window.open('https://google.com', '_blank', 'width=500, height=300')
+    window.open('https://google.com', '_blank', 'width=500, height=300')
+}
